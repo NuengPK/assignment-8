5f4fd3f4b3d1528774dcdd53ed05228b31642cd3
NuengPK <94308487+NuengPK@users.noreply.github.com>
2021-11-25 12:13:50 +0700

    การแสดงเครื่องหมายคําพูด display of quotation marks

---
+++ assignment8-3.js
@@ -0,0 +1,5 @@
+var inputName2;
+inputName2 = prompt("What is a sharp word or a proverb?","คําคม หรือ คําพังเพย อะไร?");
+var numberOfLetters2 = inputName2.length;
+console.log(inputName2, numberOfLetters);
+document.write("\"\n",inputName,"\" กล่าวว่า\" \n",inputName2),"\"\n";
