be5960c20c6923dc8cb6947c3d2ebebb1a6d9902
NuengPK <94308487+NuengPK@users.noreply.github.com>
2021-11-25 11:52:00 +0700

    Saying Hello

---
+++ assignment8-1.js
@@ -0,0 +1,5 @@
+var inputName;
+inputName = prompt("What is your name","โปรดระบุชื่อ");
+var numberOfLetters = inputName.length;
+console.log("\'Hollo\'\n",inputName, numberOfLetters);
+document.write("\'Hollo\'\n",inputName,"\' nice to miss you !\'\n");
